01990ed62d992794e446c32c0834289a371c5486
Emilio Alvarez <e.l.alvarez@icloud.com>
2017-04-22 16:36:46 -0500

    Drive Straight commands

---
+++ src/com/_2491nomythic/who/commands/DriveStraight.java
@@ -0,0 +1,51 @@
+package com._2491nomythic.who.commands;
+
+import com._2491nomythic.who.commands.CommandBase;
+
+/**
+ *
+ */
+public class DriveStraight extends CommandBase {
+	double speed;
+		
+		/**
+		 * Drives the robt straight using the gyro at a specified power
+		 */
+		public DriveStraight(double speed) {
+			// Use requires() here to declare subsystem dependencies
+			// eg. requires(chassis);
+			requires(drivetrain);
+			this.speed = speed;
+		}
+		
+		// Called just before this Command runs the first time
+		protected void initialize() {
+			drivetrain.resetGyro();
+		}
+		
+		// Called repeatedly when this Command is scheduled to run
+		protected void execute() {
+			if (speed > 0) {
+				drivetrain.drive(Math.min(speed, speed - Math.min(0.5 * speed, 0.1 * drivetrain.getRawGyroAngle())), Math.min(speed, speed + Math.max(-0.5 * speed, 0.1 * drivetrain.getRawGyroAngle())));
+			}
+			else {
+				drivetrain.drive(Math.max(speed, speed - Math.max(0.5 * speed, 0.1 * drivetrain.getRawGyroAngle())), Math.max(speed, speed + Math.min(-0.5 * speed, 0.1 * drivetrain.getRawGyroAngle())));
+			}
+		}
+		
+		// Make this return true when this Command no longer needs to run execute()
+		protected boolean isFinished() {
+			return false;
+		}
+		
+		// Called once after isFinished returns true
+		protected void end() {
+			drivetrain.stop();
+		}
+		
+		// Called when another command which requires one or more of the same
+		// subsystems is scheduled to run
+		protected void interrupted() {
+			end();
+		}
+	}
