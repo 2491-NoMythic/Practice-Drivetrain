28f10a64a442eac09a152576b38d88b6377e991c
Thomas <thetardis1968@gmail.com>
2017-11-14 17:46:53 -0600

    Revert "help"
    This reverts commit 6ac8d56277ea33d1d7cf89fa4425b1ef25dd6fa4.

---
+++ src/com/_2491nomythic/who/settings/Constants.java
@@ -0,0 +1,14 @@
+package com._2491nomythic.who.settings;
+
+public class Constants {
+	
+	// Joysticks
+	public static final int ControllerOnePort = 0;
+	
+	// CAN Channels
+	public static final int driveTalonLeft1Channel = 10;
+	public static final int driveTalonLeft2Channel = 11;
+	public static final int driveTalonRight1Channel = 12;
+	public static final int driveTalonRight2Channel = 13;
+
+}
---
+++ src/com/_2491nomythic/who/settings/ControllerMap.java
@@ -0,0 +1,10 @@
+package com._2491nomythic.who.settings;
+
+public class ControllerMap {
+	
+	public static final int driveController = 0;
+	
+	public static final int driveAxis = 1;
+	public static final int driveTurnAxis = 2;
+	
+}
