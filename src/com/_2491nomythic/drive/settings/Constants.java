5385153ae83105b512c8866269e33868a36b0755
Silas <hwalker963@gmail.com>
2018-01-13 13:39:29 -0600

    Changed a talon channel number to work with electronics in a box
    also updated jars on the electronics in a box rio

--- src/com/_2491nomythic/drive/settings/Constants.java
+++ src/com/_2491nomythic/drive/settings/Constants.java
@@ -6,7 +6,7 @@ public class Constants {
 	public static final int ControllerOnePort = 0;
 	
 	// CAN Channels
-	public static final int driveTalonLeft1Channel = 10;
+	public static final int driveTalonLeft1Channel = 14;
 	public static final int driveTalonLeft2Channel = 11;
 	public static final int driveTalonRight1Channel = 12;
 	public static final int driveTalonRight2Channel = 13;
