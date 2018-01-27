b40e0e6263213265582a59c2ee54078a03766fa6
Thomas <thetardis1968@gmail.com>
2018-01-27 11:39:16 -0600

    Changed talon values to work with prototype shooter

--- src/com/_2491nomythic/drive/settings/Constants.java
+++ src/com/_2491nomythic/drive/settings/Constants.java
@@ -6,10 +6,10 @@ public class Constants {
 	public static final int ControllerOnePort = 0;
 	
 	// CAN Channels
-	public static final int driveTalonLeft1Channel = 10;
-	public static final int driveTalonLeft2Channel = 11;
-	public static final int driveTalonRight1Channel = 12;
-	public static final int driveTalonRight2Channel = 13;
+	public static final int driveTalonLeft1Channel = 13;
+	public static final int driveTalonLeft2Channel = 12;
+	public static final int driveTalonRight1Channel = 16;
+	public static final int driveTalonRight2Channel = 19;
 	public static final int driveTalonFrontSidewaysChannel = 17;
 	public static final int driveTalonRearSidewaysChannel = 21;
 
